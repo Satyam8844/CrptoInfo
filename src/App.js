2d49eef70d29ad0b606796ede5a1bc3362af5631
Aakash Raj <aakashraj914@gmail.com>
2023-05-30 21:11:05 +0530

    adding search bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import { Box } from "@mui/material";
+import { Grid } from "@mui/material";
 import "./App.css";
 import ColumnName from "./components/ColumnName";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
-    <Box className="App">
-      <ColumnName />
-    </Box>
+    <Grid container className="App">
+      <Grid xs={6}>
+        <SearchBar />
+      </Grid>
+      <Grid xs={12}>
+        <ColumnName />
+      </Grid>
+    </Grid>
   );
 }
 
